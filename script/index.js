f0acf5019ea422f26ab955235627d20114f2200e
OnantaB <onantab47@gmail.com>
2024-02-22 10:16:39 +0100

    Menu button animation

--- script/index.js
+++ script/index.js
@@ -1,24 +1,29 @@
 let isVisible = false;
 function toggleNavbar() {
+  const menuBtn = document.querySelector(".menu-btn");
   const tog = document.querySelector(".menu-links");
   if (!isVisible) {
     tog.classList.remove("left-[-100%]");
     tog.classList.add("left-0");
+    menuBtn.classList.add("menu-open");
     console.log("opening");
     isVisible = true;
   } else {
     tog.classList.remove("left-0");
     tog.classList.add("left-[-100%]");
+    menuBtn.classList.remove("menu-open");
     console.log("closing");
     isVisible = false;
   }
 }
 
 function closeNavBarOnLinkClick() {
+  const menuBtn = document.querySelector(".menu-btn");
   const tog = document.querySelector(".menu-links");
   if (tog.classList.contains("left-0")) {
     tog.classList.remove("left-0");
     tog.classList.add("left-[-100%]");
+    menuBtn.classList.remove("menu-open");
     console.log("closing");
     isVisible = false;
   }
